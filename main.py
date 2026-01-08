05dc37776ab7fdce8ca972c6fc1a583bdd049582
Joseph Fan <pigeon@Josephs-Macbook.local>
2026-01-08 11:32:05 -0500

    Added Test.py and more input stuff

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 """
 TODO: A very useful temperature-conversion app.
 """
+def main() -> None:
+    print("Hello World")
 
-print("Hello World")
+    your_name: str = input("Give me your name please: ")
+    print(f"How you doing {your_name}?")
+
+if __name__ == "__main__":
+    main()
