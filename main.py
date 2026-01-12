bc3629701ccbd9426a25cdd1d7ebbcad3abf6441
Joseph Fan <pigeon@Josephs-Macbook.local>
2026-01-12 11:51:12 -0500

    Added is_cold_f and its tests

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 """
 TODO: A very useful temperature-conversion app.
 """
-def main() -> None:
-    """Main function"""
-    print("Hello World")
+THRESHOLD_TEMP_F: float = 68
 
+def is_cold_f(temp_f: float) -> bool:
+    """
+    Tells me if its below or upove agreed upon threshold
+
+    Args:
+        temp_f (float): temperture
+
+    Returns:
+        bool: True means below agreed upon threshold
+    """
+    return temp_f < THRESHOLD_TEMP_F
+
+def greet_person() -> None:
+    """
+    Greets the name of a person
+    from the keyboard and greets
+    them.
+    """
     your_name: str = input("Give me your name please: ")
     print(f"How you doing {your_name}?")
 
+
+def main() -> None:
+    """
+    Main function
+    """
+    pass
 if __name__ == "__main__":
     main()
--- tests.py
+++ tests.py
@@ -1,2 +1,40 @@
 import unittest
-import main
+from unittest.mock import patch, Mock
+import unittest.mock
+from main import (
+    greet_person,
+    is_cold_f,
+    THRESHOLD_TEMP_F
+)
+
+
+class TestMainApp(unittest.TestCase):
+    """
+    Class for testing my code from main
+    """
+    @patch('builtins.input', side_effect = ['alice'])
+    @patch('builtins.print')
+    def test_greetperson(self, mock_print: Mock, _:Mock) -> None:
+        """"
+        Test to test greet_person function 
+        from main
+        """
+        greet_person()
+        except_calls = [
+            unittest.mock.call("How you doing alice?")
+        ]
+        mock_print.assert_has_calls(except_calls)
+    def test_is_cold_belowthresh(self) -> None:
+        "test is for is_cold_f for below Threshold "
+        self.assertTrue(is_cold_f(THRESHOLD_TEMP_F-10))
+        
+    def test_is_cold_thresh(self) -> None:
+        "test is for is_cold_f for Threshold"
+        self.assertFalse(is_cold_f(THRESHOLD_TEMP_F))
+        
+    def test_is_cold_overthresh(self) -> None:
+        "test is for is_cold_f for above Threshold "
+        self.assertFalse(is_cold_f(THRESHOLD_TEMP_F+10))
+
+if __name__ == "__main__":
+    unittest.main()
